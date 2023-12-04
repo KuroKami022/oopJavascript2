4312597b8b936ac4e146f5233b420e61b27e9947
Kami <luisru693@gmail.com>
2023-12-04 18:27:31 -0500

    recursion

--- StaticMethodsOfObject/main.js
+++ StaticMethodsOfObject/main.js
@@ -50,3 +50,35 @@ Object.seal(juan);
 Object.freeze(juan);
 
 console.log(Object.getOwnPropertyDescriptors(juan));
+
+const obj1 = {
+  a: "a",
+  b: "b",
+  c: {
+    d: "d",
+    e: "e",
+  },
+  editA() {
+    this.a = "AAAAAA";
+  }
+};
+
+const stringifiedComplexObj = JSON.stringify(obj1);
+const obj2 = JSON.parse(stringifiedComplexObj);
+
+const numeritos = [0,1,2,3,4,5,5,6,7,8,9,435678,7,2,3];
+let numerito = 0;
+for (let index = 0; index < numeritos.length; index++) {
+  numerito = numeritos[index];
+  console.log({ index, numerito });
+}
+
+function recursiva(numbersArray) {
+  if (numbersArray.length != 0) {
+    const firstNum = numbersArray[0];
+    console.log(firstNum);
+
+    numbersArray.shift();
+    recursiva(numbersArray);
+  }
+}
